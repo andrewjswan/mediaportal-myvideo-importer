bc24ee1bf7b436d83f2e01e5d9ba01b8e9dc3bff
Andrew J.Swan <ajs.blackhole@gmail.com>
2025-06-10 20:12:56 +0300

    Bump Windows to Windows Server 2022

--- scripts/Build_GIT.bat
+++ scripts/Build_GIT.bat
@@ -3,15 +3,6 @@ cls
 Title Building MediaPortal MyVideo Importer (RELEASE)
 cd ..
 
-if "%programfiles(x86)%XXX"=="XXX" goto 32BIT
-	:: 64-bit
-	set PROGS=%programfiles(x86)%
-	goto CONT
-:32BIT
-	set PROGS=%ProgramFiles%
-:CONT
-IF NOT EXIST "%PROGS%\Team MediaPortal\MediaPortal\" SET PROGS=C:
-
 setlocal enabledelayedexpansion
 
 :: Prepare version
@@ -20,7 +11,20 @@ set REVISION=%REVISION: =%
 "scripts\Tools\sed.exe" -i "s/\$WCREV\$/%REVISION%/g" MyVideoImporter\Properties\AssemblyInfo.cs
 
 :: Build
-"%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBUILD.exe" /target:Rebuild /property:Configuration=RELEASE /fl /flp:logfile=MyVideoImporter.log;verbosity=diagnostic MyVideoImporter.sln
+FOR %%p IN ("%PROGRAMFILES(x86)%" "%PROGRAMFILES%") DO (
+  FOR %%s IN (2019 2022) DO (
+    FOR %%e IN (Community Professional Enterprise BuildTools) DO (
+      SET PF=%%p
+      SET PF=!PF:"=!
+      SET MSBUILD_PATH="!PF!\Microsoft Visual Studio\%%s\%%e\MSBuild\Current\Bin\MSBuild.exe"
+      IF EXIST "!MSBUILD_PATH!" GOTO :BUILD
+    )
+  )
+)
+
+:BUILD
+
+%MSBUILD_PATH% /target:Rebuild /property:Configuration=RELEASE /fl /flp:logfile=MyVideoImporter.log;verbosity=diagnostic MyVideoImporter.sln
 
 :: Revert version
 git checkout MyVideoImporter\Properties\AssemblyInfo.cs
